98c890c727bb6d3ea04d8d977bc392713b02cfaf
BluIsMe <soeun2388@kangnam.ac.kr>
2025-01-10 11:01:38 +0900

    Initial Commit

---
+++ src/main/java/com/example/workbee/WorkbeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.workbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkbeeApplication.class, args);
+    }
+
+}
